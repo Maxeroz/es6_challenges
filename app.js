9ab8c3c9067eb86c1756b270454b9c09589215de
Maxim <110911222+Maxeroz@users.noreply.github.com>
2022-11-06 17:12:58 +0500

    Case Insensetive Palidrome

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
-function arrayReplace(array, elemToReplace, substitutionElem) {
-    // iterate throught list
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === elemToReplace) {
-            array[i] = substitutionElem;
-        }
-    }
-    return array
+// Array Replace
+
+// function arrayReplace(array, elemToReplace, substitutionElem) {
+//     // iterate throught list
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] === elemToReplace) {
+//             array[i] = substitutionElem;
+//         }
+//     }
+//     return array
+// }
+
+// console.log(arrayReplace([1,2,1],1,3))
+
+// Case Insensetive Palidrome
+
+function caseInsensetivePalidrome(str) {
+    // make str caseless
+    const firstString = str.toLowerCase();
+    console.log(firstString);
+
+    // compare it forwards and backwards
+    const newArray = firstString.split("").reverse().join("");
+    console.log(newArray);
+    // return result
+
+    return firstString === newArray;
 }
 
-console.log(arrayReplace([1,2,1],1,3))
+console.log(caseInsensetivePalidrome("aabaa"));
