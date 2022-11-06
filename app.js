d3e5f5f0b634e6084b11dd396dac4c3ea39cf080
Maxim <110911222+Maxeroz@users.noreply.github.com>
2022-11-06 16:47:10 +0500

    Array Replace

---
+++ app.js
@@ -0,0 +1,11 @@
+function arrayReplace(array, elemToReplace, substitutionElem) {
+    // iterate throught list
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === elemToReplace) {
+            array[i] = substitutionElem;
+        }
+    }
+    return array
+}
+
+console.log(arrayReplace([1,2,1],1,3))
