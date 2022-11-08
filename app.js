12d9e5bf2732e0654743e2df516d7b535e99bb9b
Maxim <110911222+Maxeroz@users.noreply.github.com>
2022-11-08 22:25:51 +0500

    maxMulpiple

--- app.js
+++ app.js
@@ -30,11 +30,76 @@
 
 // Enclose in Brackets
 
-function encloseInBrackets(str) {
-    const solution1 = `(${str})`;
-    const solution2 = "(" + str + ")";
-    const solution3 = "(".concat(str, ")")
-    console.log(solution1);
+// function encloseInBrackets(str) {
+//     const solution1 = `(${str})`;
+//     const solution2 = "(" + str + ")";
+//     const solution3 = "(".concat(str, ")")
+//     console.log(solution1);
+// }
+
+// encloseInBrackets("Mother")
+
+// function factorialNumber(num) {
+//     for ( let i = num - 1; i >= 1; i-- ) {
+//         num = num * i;
+//     }
+// }
+// factorialNumber(5)
+
+// function factorialNumber(num) {
+//     let sum = 1;
+
+//     for ( let i = 1; i <= num; i++ ) {
+//         sum *= i;
+//     }
+//     return sum;
+// }
+
+// console.log(factorialNumber(5))
+
+// First Digit
+
+// function firstDigit(str) {
+//     const strNums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+//     const chars = str.split(" ");
+
+//     for ( const char of chars) {
+//         if ( strNums.includes(char)) {
+//             // debugger
+//             return char;
+//         }
+//     }
+// }
+
+// console.log(firstDigit("3 Hello"))
+
+// function largestNumber(num) {
+//     let placeholder = '';
+
+//     for(let i = 0; i < num; i++) {
+//         placeholder = placeholder.concat('9');
+//         // debugger
+//     }
+//     return parseInt(placeholder);   
+// }
+
+// console.log(largestNumber("2"))
+
+// function largestNumber(num) {
+//     const placeholder = "9".repeat(num)
+
+//     return parseInt(placeholder);
+// }
+
+// console.log(largestNumber("2"))
+
+// maxMulpiple
+
+function maxMulpiple(divisor, bound) {
+    const remainder = bound % divisor;
+    debugger
+    return bound - remainder;
+
 }
 
-encloseInBrackets("Mother")
+console.log(maxMulpiple(3, 10));
